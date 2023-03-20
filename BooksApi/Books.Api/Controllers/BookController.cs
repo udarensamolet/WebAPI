88073089a726023c272fefebe77c971a140e4a5b
Rositsa Nenova <udarensamolet@yahoo.com>
2023-03-20 03:34:58 +0200

    added GetAllBooksAsync method

---
+++ BooksApi/Books.Api/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Books.Core.Contracts;
+using Books.Infrastructure.Models;
+
+namespace Books.Api.Controllers
+{
+    [ApiController]
+    [Route("api/book")]
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async IAsyncEnumerable<IEnumerable<Book>> GetAllBooksAsync()
+        {
+            yield return await _bookService.GetAllBooksAsync();
+        }
+    }
+}
